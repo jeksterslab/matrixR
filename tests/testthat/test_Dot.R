014fda3eb732c77ef7f26235f7c9d075eda42eb5
jeksterslab <social.jeksterslab@gmail.com>
2021-01-29 23:04:21 +0800

    Automated update.

---
+++ tests/testthat/test_Dot.R
@@ -0,0 +1,54 @@
+#' ---
+#' title: "Test: Dot"
+#' author: "Ivan Jacob Agaloos Pesigan"
+#' date: "`r Sys.Date()`"
+#' output: rmarkdown::html_vignette
+#' vignette: >
+#'   %\VignetteIndexEntry{Test: Dot}
+#'   %\VignetteEngine{knitr::rmarkdown}
+#'   %\VignetteEncoding{UTF-8}
+#' ---
+#'
+#+ knitr_options, include=FALSE, cache=FALSE
+knitr::opts_chunk$set(
+  error = TRUE,
+  collapse = TRUE,
+  comment = "#>",
+  out.width = "100%"
+)
+#'
+#+ setup
+library(testthat)
+library(matrixR)
+context("Test Dot.")
+#'
+#+ testthat
+test_that("Dot.", {
+  expect_equal(
+    Dot(
+      c(1, 3, -5),
+      c(4, -2, -1)
+    ),
+    3
+  )
+  # yac_symbol
+  expect_equal(
+    as.numeric(
+      Dot(
+        Ryacas::ysym(c(1, 3, -5)),
+        Ryacas::ysym(c(4, -2, -1))
+      )
+    ),
+    3
+  )
+  expect_equal(
+    eval(
+      Dot(
+        Ryacas::ysym(c(1, 3, -5)),
+        Ryacas::ysym(c(4, -2, -1)),
+        str = FALSE
+      )
+    ),
+    3
+  )
+})
